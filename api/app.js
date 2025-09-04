621a1cedd40f176c3bdfd0acaafefbaf5b684dcd
rajalamdev <rajalamdev@gmail.com>
2025-09-05 04:13:37 +0700

    update

---
+++ api/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const routes = require("../routes"); // import routes/index.js
+const serverless = require("serverless-http");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Hello World 🚀");
+});
+
+// pakai semua routing dari /routes
+app.use(routes);
+
+// export sebagai serverless handler, BUKAN app.listen()
+module.exports = app;
+module.exports.handler = serverless(app);
