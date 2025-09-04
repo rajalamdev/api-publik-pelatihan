21aed7da47e78b70c2ce7612fe2c78dabb199154
rajalamdev <rajalamdev@gmail.com>
2025-09-05 04:47:17 +0700

    update bet

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const serverless = require("serverless-http");
-
-const routes = require("./routes"); // ✅ pakai ./routes
+const routes = require("./routes"); 
 
 require("dotenv").config();
 
@@ -11,13 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Pakai semua routing
-app.use("/api", routes);
-
-// Default route
 app.get("/", (req, res) => {
   res.send("Hello World 🚀 from Vercel");
 });
 
+app.use("/api", routes);
 
 module.exports = serverless(app);
